dad30950ee030c5890e2790a6c9afcb931623ca3
antpowell <powell.anthony.e@gmail.com>
2016-02-20 06:16:03 -0600

    cards locking ranking and card data captured reset button atted to reload page

--- app/scripts/priorityHelper.js
+++ app/scripts/priorityHelper.js
@@ -9,12 +9,21 @@
     $('.card').draggable();
     $('.holder').droppable({
     drop: function(ev, ui) {
-        $(ui.draggable).detach().css({top: 0,left: -35}).appendTo(this);
+      // $(ui.draggable).parent().droppable( 'option', 'disabled', false );
+        $(ui.draggable).detach().css({top: 20,left: -10}).appendTo(this);
+        $(parent).droppable({disabled:true});
+        var cardSelection = ui.draggable.attr('id');
+        var holderID = $(this).attr('id');
+        console.log(cardSelection+' : '+holderID);
+
     }
 });
   });
   $('#clkBtn').on('click',function(){
 
+  });
+  $('#resetBtn').on('click',function(){
+    location.reload();
   });
 
 })(jQuery);
