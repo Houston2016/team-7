8f0ae3fb23946a053ff0633c07dfdcdc4a751b05
antpowell <powell.anthony.e@gmail.com>
2016-02-20 03:54:19 -0600

    Visually good on tab and pc

--- app/scripts/priorityHelper.js
+++ app/scripts/priorityHelper.js
@@ -7,18 +7,11 @@
     // more code using $ as alias to jQuery
     console.log('Good');
     $('.card').draggable();
-    $('#holder').droppable({
-      over: function(event, ui) {
-        $('.card').draggable({
-            grid: [50, 50]
-        });
-        console.log(event+':'+ui);
-    },
-    out: function(event, ui) {
-        $('.card').draggable('option', 'grid', false);
-        console.log('New:'+event+':'+ui);
+    $('.holder').droppable({
+    drop: function(ev, ui) {
+        $(ui.draggable).detach().css({top: 0,left: -35}).appendTo(this);
     }
-    });
+});
   });
   $('#clkBtn').on('click',function(){
 
